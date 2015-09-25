357c6f0727bcc7dcb375eb71040984fc789954d1
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-09-24 21:56:33 -0400

    added react routers

--- app-client.js
+++ app-client.js
@@ -1,5 +1,21 @@
 var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+
 var APP = require('./components/APP');
+var Audience = require('./components/Audience');
+var Speaker = require('./components/Speaker');
+var Board = require('./components/Board');
 
-React.render(<APP />, document.getElementById('react-container'));
+var routes = (
+  <Route handler={APP}>
+    <DefaultRoute handler={Audience} />
+    <Route name="speaker" path="speaker" handler={Speaker}></Route>
+    <Route name="board" path="board" handler={Board}></Route>
+  </Route>
+);
 
+Router.run(routes, function(Handler){
+  React.render(<Handler />, document.getElementById('react-container'));
+});
